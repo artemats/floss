4cc8a28b58077d6a8d73de05cbf3d6afa8f3558c
artemats <artem.matseruk@gmail.com>
2021-12-07 11:58:35 +0200

    fix vimeo video to new video URL

--- src/js/common/loadVimeoPlayer.js
+++ src/js/common/loadVimeoPlayer.js
@@ -1,11 +1,18 @@
 export const loadVimeoPlayer = () => {
 	const iframe = document.querySelector('#vimeo-player');
+	const preUrlPath = `https://player.vimeo.com/video/`;
 	const urlPath = `?title=0&byline=0&portrait=0&transparent=1&controls=0&loop=1&responsive=1`;
-	let src = iframe.getAttribute('src');
+	let src = iframe.getAttribute('data-src');
 	if(!!src) {
-		iframe.setAttribute('src', `${src}${urlPath}`);
-		document.querySelector('.video-toggle').classList.add('enable-toggle');
-		return true;
+
+		let videoUrlArray = src.split('/');
+		let videoId = videoUrlArray.length ? videoUrlArray[3] : '';
+
+		if(!!videoId) {
+			iframe.setAttribute('src', `${preUrlPath}${videoId}${urlPath}`);
+			document.querySelector('.video-toggle').classList.add('enable-toggle');
+			return true;
+		}
 	}
 	return false;
 }
